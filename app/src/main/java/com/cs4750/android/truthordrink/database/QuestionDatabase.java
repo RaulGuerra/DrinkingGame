696b957ef1b4d68f6ea34c4652680746d040806b
leonidesu <andrewjunggg@gmail.com>
2022-11-27 22:39:40 -0800

    Add getDatabase method

--- app/src/main/java/com/cs4750/android/truthordrink/database/QuestionDatabase.java
+++ app/src/main/java/com/cs4750/android/truthordrink/database/QuestionDatabase.java
@@ -1,6 +1,9 @@
 package com.cs4750.android.truthordrink.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.cs4750.android.truthordrink.Question;
@@ -8,4 +11,20 @@ import com.cs4750.android.truthordrink.Question;
 @Database(entities = {Question.class}, version = 1)
 public abstract class QuestionDatabase extends RoomDatabase {
     public abstract QuestionDao questionDao();
+    private static QuestionDatabase INSTANCE;
+
+    static QuestionDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (QuestionDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            QuestionDatabase.class,
+                            "question_table"
+                    ).build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
